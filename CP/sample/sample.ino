8c0ac15d624882bd09a511c21bd920c9cb032a60
Soham <sohammane1904@gmail.com>
2023-12-14 14:07:24 +0530

    added sample project

--- CP/sample/sample.ino
+++ CP/sample/sample.ino
@@ -1,18 +1,19 @@
-#define BLYNK_TEMPLATE_ID "TMPL39lPL_Vf9"
-#define BLYNK_TEMPLATE_NAME "LDR Notification"
-#define BLYNK_AUTH_TOKEN "h9PnTEDJpAapUcZ3e0gyG5nHk6a9MDLi"
+#define BLYNK_TEMPLATE_ID "TMPL3Y5Hqs0fQ"
+#define BLYNK_TEMPLATE_NAME "medicine"
+#define BLYNK_AUTH_TOKEN "0n36v5e0Hoor7Ox74l-rfl4Oc8TmviZ2"
 #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
 
 char auth[]=BLYNK_AUTH_TOKEN;
 const int ldrPin = A0;
 const int led = D1;
-int threshold = 100;
-
+int threshold = 150;
+bool flag = false;
 
 void setup()
 {
   // put your setup code here, to run once:
+  // In general, the resistance of an LDR decreases as the light intensity increases and vice versa.
   Serial.begin(9600);
   Blynk.begin(auth,"Soham","iambatman");
   pinMode(ldrPin, INPUT);
@@ -25,10 +26,16 @@ void loop()
   // put your main code here, to run repeatedly:
   int rawData = analogRead(ldrPin);
   Serial.println(rawData);
-  if (rawData < threshold)
+  if(flag)
   {
+    delay(10000);
+    flag = false;
+  }
+  if (rawData > threshold)
+  {
+    flag = true;
     digitalWrite(led,HIGH);
-    Blynk.logEvent("ldr_alert","Take medicines");
+    Blynk.logEvent("medicine_alert","Take medicines");
   }
   else
   {
